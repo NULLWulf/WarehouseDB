1c0f0374cc1827af6819de59c56276809832bd50
Nathan <nwolf2@oswego.edu>
2021-11-17 17:04:56 -0500

    trying to get trygrab working

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const warehouseDB = require('./whdbs');
-console.log("App is running")
 
 global.port = process.env.HOST_PORT
 
-
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false}));
     
-const db = warehouseDB.getDbServiceInstance()                                            
+const db = warehouseDB.getDbServiceInstance()
 
-function tryGrab(){
+app.tryGrab = function (tryGrab, param2) {
+
+}
+app.tryGrab('/tryGrab', (request, response) =>{
     const db = warehouseDB.getDbServiceInstance();
     const result = db.getSql();
     console.log(result);
-}
+
+    result
+        .then(data => response.json({data : data}))
+        .catch(err => console.log(err));
+});
 
 app.listen(port, () => console.log('app is running on port ' + port + '.'));
