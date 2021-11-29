13eb04ff450e669877820ac8575a747cb1bab454
iDezzyxo <93735523+iDezzyxo@users.noreply.github.com>
2021-11-29 08:42:43 -0500

    Update ware.js

--- ware.js
+++ ware.js
@@ -2,26 +2,26 @@ document.addEventListener('DOMContentLoaded', function () {
   fetch('vincentprivatenas.mynetgear.com')
     .then(response => response.json())
     .then(data => loadHTMLTable(data['data']));
-  
+
 
 });
+/* this is for whe you press the button it should display the table*/
+document.getElementById('fetchOrders').addEventListener('click', loadHTMLTable);
 
+/* this is where the query data will get stored after grabbing from json going to have to repeat for each table i think--> */
+function fetchWareData(){
+  fetch(/*"this should be where the query goes"*/)
+  .then(response => response.json())
+  .then(query1 =>cosole.log(query1));
+}
 
-
+/* not sure about using this still yet */
 function loadHTMLTable(data) {
-    const table = document.querySelector('table tbody');
+    const table = document.querySelector('table-bodya');
     if (data.length === 0) {
         table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
 return;
     }
     let tableHtml = "";
-data.forEach(function(EmplyeeID, LastName, Position)){
-  tableHtml += "<tr>";
-  tableHtml += `<td>${EmployeeID}</td>`;
-  tableHtml += `<td>${LastName}</td>`;
-  tableHtml += `<td>${Position}</td>`;
-  tableHtml += "</tr>";
 });
-table.innerHTML = tableHtml;
-
 }
