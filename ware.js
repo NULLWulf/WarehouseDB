7dfbf255202c84b63d3cf308372869f3a35f0e14
Nathan <nwolf2@oswego.edu>
2021-12-06 05:27:34 -0500

    all build table methods needs to be passed as query.data to properly access the member inside

--- ware.js
+++ ware.js
@@ -4,13 +4,26 @@ document.getElementById('fetchOrders').addEventListener('click', function(){
     }).then(function(json) {
         let query = json;
         console.log(query);
+        buildOrderCustTable(query.data)
 
     }).catch(function(err) {
         console.log('Fetch problem: ' + err.message);
     });
-
-
 });
+
+function buildOrderCustTable(data){
+    var table = document.getElementById('myTable')
+    for (var i = 0; i < data.length; i++){
+        var row = `
+    <tr>
+             <td>${data[i].OrderID} </td>
+             <td>${data[i].CustomerID} </td>
+             <td>${data[i].RouteName} </td>
+             <td>${data[i].Region} </td>
+    </tr> `
+        table.innerHTML += row
+    }
+}
 
 document.getElementById('fetchEmployees').addEventListener('click', function(){
     fetch('/query2').then(function(response) {
